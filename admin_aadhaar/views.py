0939dca5d6e745c1333c6dc6da7fe700876f20da
KAMALPREET SINGH <73851933+Kamalpreet-3223@users.noreply.github.com>
2022-08-23 03:59:51 +0530

    working everything now

--- admin_aadhaar/views.py
+++ admin_aadhaar/views.py
@@ -1,4 +1,56 @@
 from django.shortcuts import render
+import firebase_admin
+from firebase_admin import credentials
+from django.views.decorators.csrf import csrf_exempt
+from firebase_admin import auth, firestore, db
 
+
+
+cred = credentials.Certificate("admin_aadhaar/ServiceKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+@csrf_exempt
 def home(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        age = request.POST.get('age')
+        phone = request.POST['phone']
+        email = request.POST['email']
+        pswd = request.POST['pswd']
+        gender = request.POST.get('gender')
+        centerUid = request.POST.get('centerUid')
+        latLon = request.POST.get('latLon')
+        picture = request.FILES.get('wizard-picture')
+        print(name, age, phone, email, gender, centerUid, latLon, picture, pswd)
+
+        user = auth.create_user(
+            email=email,
+            email_verified=True,
+            password=pswd,
+            display_name=name,
+            photo_url=picture,
+            disabled=False)
+        print('Sucessfully created new user: {0}'.format(user.uid))
+
+        data = {
+            "operatorId": user.uid,
+            "centerLocation": latLon,
+            "centerUid": centerUid,
+            "name": name,
+            "picture": picture,
+            "age": age,
+            "gender": gender,
+            "phoneNo": phone,
+            "email": email,
+            "ratings": "0",
+            "reviews": [],
+            "isOperatorActive": True,
+            "timestamp": firestore.SERVER_TIMESTAMP
+        }
+
+        # Add a new doc in collection 'cities' with ID 'LA'
+        db.collection(u'operators').document(user.uid).set(data)
+
+        return render(request, 'index.html')
     return render(request, 'index.html')
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+let submit = document.getElementById('save');
+
+submit.addEventListener('click', function(e) {
+    alert('Operator added Successfully');
+});
